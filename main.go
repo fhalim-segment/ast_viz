3eaf91884bbcca8deb408c207209670f2d81bff5
Fawad Halim <fawad.halim@segment.com>
2022-03-17 12:25:36 -0500

    Support generating plantuml on the CLI

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"os"
 
 	"gitlab.com/fawad/ast-viz/astviz"
+	"gitlab.com/fawad/ast-viz/journeyviz"
 )
 
 func main() {
+	procesAst := flag.Bool("a", false, "Whether the input is an AST. Defaults to processing it as a Journey")
+	flag.Parse()
 	infile := os.Stdin
 	outfile := os.Stdout
 	defer infile.Close()
@@ -18,9 +22,18 @@ func main() {
 	if err != nil {
 		log.Fatal("Unable to read input", err)
 	}
-	v, err := astviz.Load(bytes)
-	if err != nil {
-		log.Fatal("Unable to process input", err)
+	if *procesAst {
+		v, err := astviz.Load(bytes)
+		if err != nil {
+			log.Fatal("Unable to process AST input", err)
+		}
+		v.Dump(outfile, 0)
+	} else {
+		v, err := journeyviz.Load(bytes)
+		if err != nil {
+			log.Fatal("Unable to process Journey input", err)
+		}
+		v.Dump(outfile, 0)
 	}
-	v.Dump(outfile, 0)
+
 }
